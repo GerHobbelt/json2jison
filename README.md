4cbe8950a1fda78a3eb050841f275f14c78790d2
Ger Hobbelt <ger@hobbelt.com>
2017-02-19 16:35:47 +0100

    migrated all tests to mocha+chai (in-browser test mode via tests/index.html is not working yet, but that wasn't available before either, so nothing is lost. All tests pass in node via `make` or `make test`)

--- README.md
+++ README.md
@@ -6,9 +6,11 @@
 
 [jison](http://jison.org) grammars come in two formats: JSON or a special text format similar to Bison's. This utility converts from the JSON format to jison's text format. See the [jison2json](https://github.com/zaach/jison2json) utility for the reverse conversion.
 
+
 ## install
 
     npm install json2jison -g
+
 
 ## license
 
--- tests/tests.js
+++ tests/tests.js
@@ -1,17 +1,24 @@
-// TODO real tests
-var assert = require("better-assert");
-var json2jison = require('./json2jison');
-var grammar = "%% foo : bar { return true; } ;";
-var json = {
-  "bnf": {
-    "foo": [
-      [
-        "bar",
-        " return true; "
-      ]
-    ]
-  }
-};
+var assert = require("chai").assert;
+var json2jison = require('../json2jison.js');
 
-var rv = json2jison.convert(json);
-assert.equal(rv.trim().replace(/\s+/g, ' '), grammar.replace(/\s+/g, ' '));
+// TODO real tests
+
+describe("JSON2JISON", function () {
+  it("should be able to convert an appropriate JSON file to a JISON grammar", function () {
+    var grammar = "%% foo : bar { return true; } ;";
+    var json = {
+      "bnf": {
+        "foo": [
+          [
+            "bar",
+            " return true; "
+          ]
+        ]
+      }
+    };
+
+    var rv = json2jison.convert(json);
+    assert.equal(rv.trim().replace(/\s+/g, ' '), grammar.replace(/\s+/g, ' '));
+  });
+});
+
